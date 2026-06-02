a1f632a69fccfc125853f424b850c1258c14765e
Bruno-Manuel <brunomanuelcg@gmail.com>
2026-06-01 21:52:58 -0600

    Update ds_c4sc4_rub.py

--- ds_c4sc4_rub.py
+++ ds_c4sc4_rub.py
@@ -44,7 +44,7 @@ rango_desempeno = st.slider(
     value=(1.0, 5.0), # Valores iniciales por defecto (mínimo, máximo)
     step=0.1          # Permite avanzar de décima en décima
 )
-selected_marital_status = st.selectbox("Selecciona el estado civil", employee_data["marital_status"].unique())
+selected_marital_status = st.selectbox("Selecciona el estado civil", data["marital_status"].unique())
 
 
 data_filtrada = data[
